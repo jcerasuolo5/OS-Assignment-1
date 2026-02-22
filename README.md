0a7e0aaf0bc984df22359d4f47a7533feeed5964
Jasmine Cerasuolo <jcerasuolo@csu.fullerton.edu>
2026-02-21 16:02:31 -0800

    Lots of fixes and final draft, also fixed the readme doc

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
-# OS-Assignment-1
-Operating Systems CPSC 351 Assignment 1
+## Information
+Name: Jasmine Cerasuolo
+Email: jcerasuolo@csu.fullerton.edu
+Github User: jcerasuolo5
+ 
+Name: 
+Email:
+Github User:
+ 
+### Description of Process Roles
+
+- The parent process creates Child 1.
+- Child 1 creates Child 2 and Child 3.
+- Child 2 creates Child 4 and Child 5.
+- Child 3 creates Child 6 and Child 7.
+- Child 4–7 terminate immediately after execution.
+- After each process is executed, it will be print to the terminal
+
+---
+
+## Key Concepts Demonstrated
+
+### 1. Fork System Call
+
+The `fork()` system call 
+
+- The child process receives a return value of `0` from `fork()`.
+- The parent process receives the PID of the child process.
+
+---
+
+### 2. Process Identification
+
+Each process prints:
+
+- Its own Process ID (PID) using `getpid()`
+- Its Parent Process ID (PPID) using `getppid()`
+
+These values help demonstrate correct process hierarchy.
+
+---
+
+### 3. Process Synchronization
+
+The program uses the `wait()` system call to ensure:
+
+- Parent processes wait for child processes to terminate before continuing execution.
+- This prevents zombie processes from being created.
+
+---
+
+
+## Development Environment
+
+This program was developed and tested using:
+
+- Linux-based VM environment (Tuffix)
+- GNU Compiler Collection (GCC/G++)
+- C++ programming language
+
+---
+
+## Compilation/Running Instructions
+To compile the program, use the following command:
+
+```bash
+g++ processes.cpp -o processes
+
+To Run the program, use the following command:
+./processes
+
+
+
+
+
+
+
+
--- processes.cpp
+++ processes.cpp
@@ -1,138 +1,129 @@
 #include <iostream>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <cstdlib>
 
+void child1();
+void child2();
+void child3();
+void child4();
+void child5();
+void child6();
+void child7();
 
-
-using namespace std;
-
-class Node {
-    public:
-//constructor
-    Node(int data){
-        data_ = data;
-        child2_ = nullptr;
-        child1_ = nullptr;
-        parent = nullptr;
-        pid_;
-    }
-    ////maybe call this one fork
-    Node(int data, Node* parent){
-   
-        data_ = data;
-        child2_ = nullptr;
-        child1_ = nullptr;
-        parent = parent*;
-        pid_;
-    }
-
-    int getData(){
-            return data_;
-        }
-    void setData(int data){
-            data_ = data;
-        }
-    int getParent(){
-        return parent;
-    }
-    void setParent(Node* parent){
-        parent_ = parent;
-    }
-    int getPid (){
-        return pid_;
-    }
-    void setPid(int pid_){
-        pid_ = pid;
-    }
-    private:
-        int data_;
-        Node* child1_;
-        Node* child2_;
-        Node* parent_;
-        int pid_;
-};
-
-
-// i dont think we can use node because we are supposed to be using  fork() to create the tree 
-v
 // 1. The parent creates child process Child 1
-void parent() {
-    std:: cout << "I am the original parent; my process ID is pid = 1966"<< getpid() << "\n";  
-   
+void parent()
+{
+    std::cout << "I am the original parent; my process ID is pid = " << getpid() << "\n";
+
     pid_t pid = fork();
 
-if (pid == 0) {
-    child1();
-    exit(0);
-}
-else {
+    if (pid == 0)
+    {
+        child1();
+        exit(0);
+    }
     wait(NULL);
- } 
 }
-// 2. child 1 process creates child processes Child 2 and Child 3 
+// 2. child 1 process creates child processes Child 2 and Child 3
 // (using fork()) and then waits for both to terminate (using wait());
 
-void child1() {
-    std::cout <<"I am child1 pid=1967 "<< getpid()<< "; my parent is pid= 1966" << getppid()<< "\n";
+void child1()
+{
+    std::cout << "I am child pid = " << getpid() << "; my parent is pid = " << getppid() << "\n";
 
     pid_t chi2 = fork();
 
-if (chi2 == 0) {
-    child2();
-    exit(0);
-}
+    if (chi2 == 0)
+    {
+        child2();
+        exit(0);
+    }
 
     pid_t chi3 = fork();
 
-if (chi3 == 0) {
-    child3();
-    exit(0);
-}
-else {
+    if (chi3 == 0)
+    {
+        child3();
+        exit(0);
+    }
+
     wait(NULL);
- }
+    wait(NULL);
 }
 
 // 3. Child 2 creates child processes Child 4 and Child 5 and waits for both to terminate;
-void child2() {
-    std::cout <<" I am child2 pid=1968 " <<c getpid() << "; my parent is pid=1967 " << getppid()<< "\n";
-    
+void child2()
+{
+    std::cout << "I am child pid = " << getpid() << "; my parent is pid = " << getppid() << "\n";
+
     pid_t chi4 = fork();
 
-if (chi4 == 0) {
-    child4();
-    exit(0);
-}
-
+    if (chi4 == 0)
+    {
+        child4();
+        exit(0);
+    }
+wait(NULL);
     pid_t chi5 = fork();
 
-if (chi5 == 0) {
-    child5();
-    exit(0);
-} else {
+    if (chi5 == 0)
+    {
+        child5();
+        exit(0);
+    }
+    
     wait(NULL);
- } 
 }
 
-//4.Child 3 creates child processes Child 6 and Child 7 and waits for both to terminate..Child 3 creates child processes Child 6 and Child 7 and waits for both to terminate.
-void child3() {
-    std::cout <<"I am child3 pid=1969" << getpid()<< "; my parent is pid=1967 " << getppid()<< "\n";
- 
- pid_t chi6 = fork();
+// 4.Child 3 creates child processes Child 6 and Child 7 and waits for both to terminate..Child 3 creates child processes Child 6 and Child 7 and waits for both to terminate.
+void child3()
+{
+    std::cout << "I am child pid = " << getpid() << "; my parent is pid = " << getppid() << "\n";
 
-if (chi6 == 0) {
-    child6();
-    exit(0);
-}
+    pid_t chi6 = fork();
 
+    if (chi6 == 0)
+    {
+        child6();
+        exit(0);
+    }
+ wait(NULL);
     pid_t chi7 = fork();
 
-if (chi7 == 0) {
-    child7();
-    exit(0);
-} else {
+    if (chi7 == 0)
+    {
+        child7();
+        exit(0);
+    }
+   
     wait(NULL);
- } 
 }
 
+void child4()
+{
+    std::cout << "I am child pid = " << getpid() << "; my parent is pid = " << getppid() << "\n";
+    exit(0);
+}
+void child5()
+{
+    std::cout << "I am child pid = " << getpid() << "; my parent is pid = " << getppid() << "\n";
+    exit(0);
+}
+void child6()
+{
+    std::cout << "I am child pid = " << getpid() << "; my parent is pid = " << getppid() << "\n";
+    exit(0);
+}
+void child7()
+{
+    std::cout << "I am child pid = " << getpid() << "; my parent is pid = " << getppid() << "\n";
+    exit(0);
+}
+
+int main()
+{
+    parent();
+
+    return 0;
+}
